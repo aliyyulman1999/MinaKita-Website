4204cc56337b665c5fc0cfa63ce42bfb214473ce
aliyyulman1999 <aliyyulmanj@gmail.com>
2021-02-28 10:19:32 +0700

    first committtt

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+var app_fireBase = {};
+(function(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyBuMSfGbs9EAeTV76IGgxRezb5lVx-D52c",
+    authDomain: "minakita-app.firebaseapp.com",
+    databaseURL: "https://minakita-app.firebaseio.com",
+    projectId: "minakita-app",
+    storageBucket: "minakita-app.appspot.com",
+    messagingSenderId: "526377078400",
+    appId: "1:526377078400:web:7d18575b49ad5854daa065"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+})();
